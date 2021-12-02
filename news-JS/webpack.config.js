0db525bc8ce9fd2c4b5c957de4873d0ddde8a0f3
Pavel Puchinski <pavelguest@yandex.ru>
2021-12-03 00:43:53 +0300

    feat: add tsloader, typescript, tsconfig, eslint for typescript

--- news-JS/webpack.config.js
+++ news-JS/webpack.config.js
@@ -12,10 +12,15 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+              test: /\.[tj]s$/,
+              use: 'ts-loader',
+              exclude: /node_modules/,
+            },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js', '.ts'],
     },
     output: {
         filename: 'index.js',
