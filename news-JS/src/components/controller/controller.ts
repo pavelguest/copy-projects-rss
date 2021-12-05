7238bea49bee201fdbb2a2a33501bd0198c0d0cf
Pavel Puchinski <pavelguest@yandex.ru>
2021-12-05 22:03:40 +0300

    feat: add generics for callback and add as for type

--- news-JS/src/components/controller/controller.ts
+++ news-JS/src/components/controller/controller.ts
@@ -1,7 +1,9 @@
+import { Data } from 'components/view/appView';
 import AppLoader from './appLoader';
+import { Callback } from './loader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: Callback<Data>) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,15 +12,15 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: Callback<Data>) {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
-                    newsContainer.setAttribute('data-source', sourceId);
+                    newsContainer.setAttribute('data-source', sourceId!);
                     super.getResp(
                         {
                             endpoint: 'everything',
@@ -31,7 +33,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
